0865a7f87ad31ba55a84b87961733bdcfeb07705
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2019-10-03 00:04:27 -0500

    Finished refactoring. It's missing the point bug

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
-const button0 = document.getElementById('btn-0');
-const button1 = document.getElementById('btn-1');
-const button2 = document.getElementById('btn-2');
-const button3 = document.getElementById('btn-3');
-const button4 = document.getElementById('btn-4');
-const button5 = document.getElementById('btn-5');
-const button6 = document.getElementById('btn-6');
-const button7 = document.getElementById('btn-7');
-const button8 = document.getElementById('btn-8');
-const button9 = document.getElementById('btn-9');
+const numberButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(element => document.getElementById(`btn-${element}`));
+numberButtons.map((numberButton, index) => numberButton.addEventListener('click', () => numbersClick(index)));
+
 const equalButton = document.getElementById('equal');
 const periodButton = document.getElementById('period');
 
@@ -22,20 +15,16 @@ const plusButton = document.getElementById('btn-plus');
 
 const numbersClick = number => {
   const actualText = inputString.textContent;
-
-  const result = actualText === '0' && number === '0'
+  inputString.textContent = actualText === '0' && number === '0'
     ? '0'
     : actualText === '0'
     ? number
     : `${actualText}${number}`;
-
-  inputString.textContent = result;
 };
 
 const equalClick = () => {
   const expression = inputString.textContent.replace('x', '*');
-  const result = math.evaluate(expression);
-  resultString.textContent = result;
+  resultString.textContent = math.evaluate(expression);
 };
 
 const periodClick = () => {
@@ -47,34 +36,15 @@ const delClick = () => inputString.textContent = 0;
 
 const setOperatorText = operator => {
   const actualText = inputString.textContent;
-  const regex = RegExp('(.*[0-9])[x,/,-,+]$');
-  const result = regex.test(actualText)
-      ? `${actualText.slice(0, -1)}${operator}`
-      : `${actualText}${operator}`;
-  inputString.textContent = result;
+  const regex = RegExp('(.*[0-9])[-,x,/,+]$');  
+  inputString.textContent = `${regex.test(actualText) ? actualText.slice(0, -1) : actualText}${operator}`;
 };
-
-const divClick = () => setOperatorText('/');
-const timesClick = () => setOperatorText('x');
-const subClick = () => setOperatorText('-');
-const plusClick = () => setOperatorText('+');
-
-button0.addEventListener('click', () => numbersClick(0));
-button1.addEventListener('click', () => numbersClick(1));
-button2.addEventListener('click', () => numbersClick(2));
-button3.addEventListener('click', () => numbersClick(3));
-button4.addEventListener('click', () => numbersClick(4));
-button5.addEventListener('click', () => numbersClick(5));
-button6.addEventListener('click', () => numbersClick(6));
-button7.addEventListener('click', () => numbersClick(7));
-button8.addEventListener('click', () => numbersClick(8));
-button9.addEventListener('click', () => numbersClick(9));
 
 equalButton.addEventListener('click', equalClick);
 periodButton.addEventListener('click', periodClick);
-
 delButton.addEventListener('click', delClick)
-divButton.addEventListener('click', divClick)
-timesButton.addEventListener('click', timesClick)
-subButton.addEventListener('click', subClick)
-plusButton.addEventListener('click', plusClick)
+
+divButton.addEventListener('click', () => setOperatorText('/'));
+timesButton.addEventListener('click', () => setOperatorText('x'));
+subButton.addEventListener('click', () => setOperatorText('-'));
+plusButton.addEventListener('click', () => setOperatorText('+'));
